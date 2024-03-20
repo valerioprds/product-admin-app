217664aba370fd6119dad73120139ec8346d7e11
valerioprds <v.paradiso89@gmail.com>
2024-03-20 17:33:56 +0100

    angular fire module error solved

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDwCy5vrz3rWosmrByMHPgDrLz0kBhv0zY',
+    authDomain: 'product-admin-app-2bb57.firebaseapp.com',
+    projectId: 'product-admin-app-2bb57',
+    storageBucket: 'product-admin-app-2bb57.appspot.com',
+    messagingSenderId: '1013865139210',
+    appId: '1:1013865139210:web:0ba2256aecc425dfc3e3f3',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDwCy5vrz3rWosmrByMHPgDrLz0kBhv0zY',
+    authDomain: 'product-admin-app-2bb57.firebaseapp.com',
+    projectId: 'product-admin-app-2bb57',
+    storageBucket: 'product-admin-app-2bb57.appspot.com',
+    messagingSenderId: '1013865139210',
+    appId: '1:1013865139210:web:0ba2256aecc425dfc3e3f3',
+  },
 };
 
 /*
